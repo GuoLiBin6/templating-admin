5e32a6013d4f07c7145f76465abca3675895c019
guolibin <782518577@qq.com>
2018-04-10 15:42:35 +0800

    修改了menu组件的鼠标指针，图标

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -7,23 +7,31 @@ var $menu = (function () {
         var $menu = $(e.target).parents('li');
         var $menu_bar = $menu.find('.menu-bar');
         var $menu_items = $menu.find('.menu-item');
-
+        var $arrow = $menu.find('span+i');
+        
         if ($lastBar === null) {
             $menu_items.css('display', 'block');
             $menu_bar.attr('class', 'menu-bar menu-bar-active');
+            $arrow.attr('class','iconfont icon-triangle-up');
             $lastBar = $menu;
         } else if ($lastBar.get(0) == $menu.get(0)) {
             $menu_items.css('display', 'none');
             $menu_bar.attr('class', 'menu-bar');
+            $arrow.attr('class','iconfont icon-triangle-down');
+            
             $lastBar = null;
         } else {
             $lastBar.find('.menu-item').css('display', 'none');
             $lastBar.find('.menu-bar').attr('class', 'menu-bar');
             $menu_items.css('display', 'block');
             $menu_bar.attr('class', 'menu-bar menu-bar-active');
+
+            $arrow.attr('class','iconfont icon-triangle-up');
+            $lastBar.find('span+i').attr('class','iconfont icon-triangle-down');
             $lastBar = $menu;
         }
 
+        
     });
     $menu_item.click(function (e) {
         var $item = $(e.currentTarget);
